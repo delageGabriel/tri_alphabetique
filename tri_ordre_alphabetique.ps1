764c37d103c9dd6bbc9a00613b3dc484995c28aa
delageGabriel <71062337+delageGabriel@users.noreply.github.com>
2022-04-17 15:22:59 +0200

    Première version du triage par ordre alphabétique
    Ajout de la méthode pour trier par ordre alphabétique;
    Future mise à jour : rajout d'une option pour regarder les prochaines lettres si jamais les premières sont semblables;
    Future mise à jour : correction de bugs;

--- tri_ordre_alphabetique.ps1
+++ tri_ordre_alphabetique.ps1
@@ -33,9 +33,9 @@ for($i = 0; $i -lt $tailleTableau ; $i++)
 #                   ** Création tableaux alphabet **                        #
 #############################################################################
 
-$tableauAlphabetMajuscule = @('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-$tableauAlphabetMinuscule = @('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
-$tableauChiffreMajMin     =  @(25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0)
+$tableauAlphabetMajuscule = @('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','')
+$tableauAlphabetMinuscule = @('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','')
+$tableauChiffreMajMin     = @(26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0)
 
 #############################################################################
 #     ** Création d'une méthode pour extraire des caractères **             #
@@ -52,3 +52,80 @@ function RecupCaractere([string]$mot, [int]$index, [int]$nbCaractereARecup)
 #       ** Création d'une méthode qui va chercher la lettre dans le **      #
 #       ** tableau d'alphabet, et l'associer à un chiffre           **      #
 #############################################################################
+
+function CompareLettreChiffre([char]$lettre)
+{
+    for($i = 0 ; $i -lt $tableauAlphabetMajuscule.Count ; $i++)
+    {
+        if($lettre -eq $tableauAlphabetMajuscule[$i])
+        {
+            return $tableauChiffreMajMin[$i]
+        }
+        else
+        {
+            for($j = 0 ; $j -lt $tableauAlphabetMinuscule.Count ; $j++)
+            {
+                if($lettre -eq $tableauAlphabetMinuscule[$j])
+                {
+                    return $tableauChiffreMajMin[$j]
+                }                    
+            }
+        }
+    }
+
+}
+
+#############################################################################
+$compteARebours = $list.Count
+$nouvelleListe  = New-Object 'System.Collections.Generic.List[string]'
+function TriAlphabetique([int]$cpt, $uneListe, $nouvelleListeMethode)
+{
+    $motLePlusGrand = $null
+    $i = 0
+    while($i -lt $cpt)
+    {
+        $k = 0
+        for($j = 0 ; $j -lt $uneListe.Count ; $j++)
+        {           
+            
+            if($motLePlusGrand -eq $null)
+            {
+                $motLePlusGrand = $uneListe[$j]
+            }
+            if($k -eq $j)
+            {
+                $k = $k + 1
+            }
+            else
+            {
+                $motAComparer = $uneListe[$k]
+            }
+
+            $motAComparer     = $uneListe[$k]
+            $lettrePlusGrande = RecupCaractere -mot $motLePlusGrand -index 0 -nbCaractereARecup 1
+            $chiffrePlusGrand = CompareLettreChiffre -lettre $lettrePlusGrande
+            $lettreAComparer  = RecupCaractere -mot $motAComparer -index 0 -nbCaractereARecup 1
+            $chiffreAComparer = CompareLettreChiffre -lettre $lettreAComparer
+
+            if($chiffrePlusGrand -lt $chiffreAComparer)
+            {
+                $motLePlusGrand = $motAComparer
+            }
+        }
+        
+        foreach($object in $uneListe)
+        {
+            if($motLePlusGrand -eq $object)
+            {
+                $uneListe.Remove($object)
+            }
+        }
+
+        $nouvelleListeMethode.Add($motLePlusGrand)
+        $motLePlusGrand = $null
+        $i = $i + 1
+    }
+}
+
+TriAlphabetique -cpt $list.Count -uneListe $list -nouvelleListeMethode $nouvelleListe
+$nouvelleListe
